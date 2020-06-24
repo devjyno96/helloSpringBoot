b784848bf2e3e14af201cba5e7a0b57ec193ca60
Jyno <jinho9613@naver.com>
2020-06-24 18:46:57 +0900

    sql save

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,13 @@
 insert into customer(name, age, active) values ("alice", 50, true);
 insert into customer(name, age, active) values ("bob", 20, false);
 insert into customer(name, age, active) values ("charlie", 30, true);
-insert into product(name, age, active) values ("charlie", 30, true);
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기1", "washer", 1000 , "삼성",20 ,"라스트 세탁기");	
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기2", "washer", 1000 , "삼성",20 ,"라스트 세탁기");
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기3", "washer", 1000 , "삼성",20 ,"라스트 세탁기");	
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기4", "washer2", 1000 , "삼성",20 ,"라스트 세탁기");	
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기5", "washer2", 1000 , "삼성",20 ,"라스트 세탁기")	;
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기6", "washer2", 1000 , "삼성",20 ,"라스트 세탁기")	;
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기7", "washer3", 1000 , "삼성",20 ,"라스트 세탁기")	;
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기8", "washer3", 1000 , "삼성",20 ,"라스트 세탁기")	;
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기9", "washer3", 1000 , "삼성",20 ,"라스트 세탁기")	;
+insert into product ( name, category, price, manufacturer, unitinstock, description) values ("세탁기10", "washer4", 1000 , "삼성",20 ,"라스트 세탁기");	
